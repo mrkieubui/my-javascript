cda0a9440ece545aff9861d8462be542305cd841
Thanh Bui <40458183+mrkieubui@users.noreply.github.com>
2021-05-29 21:28:52 +0700

    Create my-array.js
    ok

---
+++ my-array.js
@@ -0,0 +1,26 @@
+var danhSachLop = [
+  {
+    name: 'Thanh',
+    age: 30,
+    gender: 'male'
+    phone'012345,
+  },
+  {
+    name: 'Hoa',
+    age: 30,
+    gender: 'female'
+    phone'012345678,
+  },
+  {
+    name: 'Ha',
+    age: 30,
+    gender: 'male'
+    phone'012345789,
+  },
+  {
+    name: 'Noi',
+    age: 30,
+    gender: 'female'
+    phone'012345555,
+  }
+];
